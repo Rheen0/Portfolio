8423f645a0b3eb8345993f8480b4a557300c5f3a
Rhandie <76822279+Rheen0@users.noreply.github.com>
2024-05-25 14:58:49 +0800

    added slide animations

--- script.js
+++ script.js
@@ -46,6 +46,36 @@ educationalbg.forEach(box => {
 })
 
 
+// Start of reveal animation
+const reveal = gsap.utils.toArray('.reveal');
+reveal.forEach((text, i) => {
+    ScrollTrigger.create({
+        trigger: text,
+        toggleClass: 'active',
+        start: "top 90%",
+        end: "top 20%",
+        // markers: true,
+    })
+})
+
+const images = gsap.utils.toArray('img');
+images.forEach((img, i) => {
+    ScrollTrigger.create({
+        trigger: img,
+        toggleClass: 'active',
+        start: "top 90%",
+        end: "top 20%",
+        // markers: true,
+    })
+})
+
+
+
+// End of reveal animation
+
+
+
+
 // // Projects Animation
 // const projects = document.querySelectorAll('.project');
 
@@ -75,7 +105,7 @@ const lineAnimation = gsap.timeline({
     scrollTrigger: {
         trigger: "#education",
         start: "top center",
-        end: "top",
+
         scrub: 1,
 
 
